a2365f7528de2087041884ef876ef5c9f95192e9
leelavathikm1 <leelavathikm1@gmail.com>
2026-02-18 18:45:42 +0530

    Router-Authentication

--- React-JS/app18-React-Router-Dom/src/App.jsx
+++ React-JS/app18-React-Router-Dom/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Login } from "./Components/Login";
+import { Dashboard } from "./Components/Dashboard";
+import { Register } from "./Components/Register";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,7 +14,9 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route path="login" element={Login} />
+          <Route path="login" element={<Login />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
