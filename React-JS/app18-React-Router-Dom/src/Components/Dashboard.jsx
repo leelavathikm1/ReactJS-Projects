7343923a12909ce6d060f14fec29ec3b40fb526f
leelavathikm1 <leelavathikm1@gmail.com>
2026-02-19 19:21:13 +0530

    authentica

--- React-JS/app18-React-Router-Dom/src/Components/Dashboard.jsx
+++ React-JS/app18-React-Router-Dom/src/Components/Dashboard.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const isUserValid = localStorage.getItem("isUserValid");
+    if (!isUserValid) {
+      alert("you are not valid user");
+      navigate("/login");
+    }
+  }, []);
   return <div>Dashboard</div>;
 };
--- React-JS/app18-React-Router-Dom/src/Components/Login.jsx
+++ React-JS/app18-React-Router-Dom/src/Components/Login.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-  const [user, setUser] = React.useState({
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
     userName: "",
     password: "",
   });
   const handleChange = (e) => {
-    console.log(e.target.value);
-    const { name, value } = e.target;
-    setUser({
-      ...user,
-      [name]: value,
-    });
+    // console.log(e.target.value);
+    const newUser = { ...user };
+    newUser[e.target.name] = e.target.value;
+    setUser(newUser);
   };
-  const handleLogin = (e) => {
-    e.preventDefault();
+
+  const handleLogin = () => {
+    // e.preventDefault();
+    const validUser = {
+      userName: "admin",
+      password: "admin123",
+    };
     console.log(user);
+    if (
+      validUser.userName === user.userName &&
+      validUser.password === user.password
+    ) {
+      console.log("yes you are valid user");
+      localStorage.setItem("isUserValid", true);
+      navigate("/dashboard");
+    } else {
+      console.log("Invalid User");
+      localStorage.setItem("isUserValid", false);
+    }
   };
   return (
     <div>
