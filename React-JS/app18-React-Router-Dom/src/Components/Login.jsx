71603c4d711e8093660e69d97735026591b0a1d9
leelavathikm1 <leelavathikm1@gmail.com>
2026-02-19 18:30:04 +0530

    Authentication

--- React-JS/app18-React-Router-Dom/src/Components/Login.jsx
+++ React-JS/app18-React-Router-Dom/src/Components/Login.jsx
@@ -1,5 +1,45 @@
 import React from "react";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [user, setUser] = React.useState({
+    userName: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+  return (
+    <div>
+      <form>
+        <label htmlFor="">User Name : </label>
+        <input
+          type="text"
+          name="userName"
+          value={user.userName}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <label htmlFor="">Password : </label>
+        <input
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />{" "}
+        <br />
+        <button onClick={handleLogin} type="button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
